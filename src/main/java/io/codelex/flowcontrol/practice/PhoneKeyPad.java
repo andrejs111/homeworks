14466c7f3ebe2ceed0029066920e94cbe83201cd
andrejs111-github <andrejs.dorodnovs@gmail.com>
2023-05-15 03:21:34 +0300

    switch corrections

--- src/main/java/io/codelex/flowcontrol/practice/PhoneKeyPad.java
+++ src/main/java/io/codelex/flowcontrol/practice/PhoneKeyPad.java
@@ -11,35 +11,16 @@ public class PhoneKeyPad {
         for (int i = 0; i < sentence.length(); i++) {
             char letter = sentence.charAt(i);
             switch (letter) {
-                case 'a': case 'b': case 'c':
-                    System.out.print(2);
-                    break;
-                case 'd': case 'e': case 'f':
-                    System.out.print(3);
-                    break;
-                case 'g': case 'h': case 'i':
-                    System.out.print(4);
-                    break;
-                case 'j': case 'k': case 'l':
-                    System.out.print(5);
-                    break;
-                case 'm': case 'n': case 'o':
-                    System.out.print(6);
-                    break;
-                case 'p': case 'q': case 'r': case 's':
-                    System.out.print(7);
-                    break;
-                case 't': case 'u':case 'v':
-                    System.out.print(8);
-                    break;
-                case 'w': case 'x': case 'y': case 'z':
-                    System.out.print(9);
-                    break;
-                case ' ':
-                    System.out.print('-');
-                    break;
-                default:
-                    System.out.println("These letters can't be added" + letter);
+                case 'a','b','c' -> System.out.print(2);
+                case 'd','e','f' -> System.out.print(3);
+                case 'g','h','i' -> System.out.print(4);
+                case 'j','k','l' -> System.out.print(5);
+                case 'm','n','o' -> System.out.print(6);
+                case 'p','q','r','s' -> System.out.print(7);
+                case 't','u','v' -> System.out.print(8);
+                case 'w','x','y','z' -> System.out.print(9);
+                case ' ' -> System.out.print('-');
+                default -> System.out.println("These letters can't be added" + letter);
             }
         }
 
--- src/main/java/io/codelex/flowcontrol/practice/PrintDayInWord.java
+++ src/main/java/io/codelex/flowcontrol/practice/PrintDayInWord.java
@@ -8,31 +8,16 @@ public class PrintDayInWord {
         if (input.hasNextInt()) {
         int number = input.nextInt();
             switch (number) {
-                case 1:
-                    System.out.println("Monday");
-                    break;
-                case 2:
-                    System.out.println("Tuesday");
-                    break;
-                case 3:
-                    System.out.println("Wednesday");
-                    break;
-                case 4:
-                    System.out.println("Thursday");
-                    break;
-                case 5:
-                    System.out.println("Friday");
-                    break;
-                case 6:
-                    System.out.println("Saturday");
-                    break;
-                case 7:
-                    System.out.println("Sunday");
-                    break;
-            }
-    } else {
-            System.out.println("Not a valid day. Please enter a number from 1 to 7.");
+                case 1 -> System.out.println("Monday");
+                case 2 -> System.out.println("Tuesday");
+                case 3 -> System.out.println("Wednesday");
+                case 4 -> System.out.println("Thursday");
+                case 5 -> System.out.println("Friday");
+                case 6 -> System.out.println("Saturday");
+                case 7 -> System.out.println("Sunday");
+                }
+                    } else {
+                        System.out.println("Not a valid day. Please enter a number from 1 to 7.");
         }
     }
-
 }
